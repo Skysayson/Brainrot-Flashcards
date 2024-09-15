7933a7088f66552275d66f0f5ef9665067c73e8d
Jonaz Juan Sayson <123169211+Skysayson@users.noreply.github.com>
2024-09-15 04:01:53 -0700

    added core functionality for flash cards

--- FlashingCards/app/(tabs)/index.tsx
+++ FlashingCards/app/(tabs)/index.tsx
@@ -1,70 +1,62 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
+import { useState } from "react";
+import FlashCardConstructor from "../../constructor/FlashCardConstructor";
+import { Button } from "react-native";
 
 export default function HomeScreen() {
+  const [flip, isFlipped] = useState(false);
+  const [x, setX] = useState(0);
+  const flashCard1 = new FlashCardConstructor("Question1", "Answer1");
+  const flashCard2 = new FlashCardConstructor("Question2", "Answer2");
+  const flashCard3 = new FlashCardConstructor("Question3", "Answer3");
+  const flashCard4 = new FlashCardConstructor("Question4", "Answer4");
+  const flashCard5 = new FlashCardConstructor("Question5", "Answer5");
+
+  const cardSet = [flashCard1, flashCard2, flashCard3, flashCard4, flashCard5];
+
+  const flipCard = () => {
+    flip == true ? isFlipped(false) : isFlipped(true);
+  };
+
+  const nextCard = () => {
+    if (x < cardSet.length - 1) {
+      setX(x + 1);
+    } else {
+      setX(0);
+    }
+  };
+
+  const prevCard = () => {
+    if (x > 0) {
+      setX(x - 1);
+    }
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Button title="Prev" onPress={prevCard} />
+
+      <Text
+        style={{ borderWidth: 1, padding: 100, margin: 10 }}
+        onPress={flipCard}
+      >
+        {flip ? (
+          <Text>{cardSet[x].back}</Text>
+        ) : (
+          <Text>{cardSet[x].front}</Text>
+        )}
+      </Text>
+
+      <Button title="Next" onPress={nextCard} />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
